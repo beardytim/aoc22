3fe935c76d1c5f2e55e3e0b3bb25d3b190c18f79
Tim Simmonds <tim.simmonds@bjss.com>
2022-12-05 14:45:01 +0000

    day 4

--- days/day_4.dart
+++ days/day_4.dart
@@ -1,8 +1,19 @@
 import '../inputs/day_4.dart';
 
 void main() {
-  List<String> input = sampleInput; // = properInput;
-  
-  
-  
+  List<String> input = properInput;
+
+  int count = 0;
+  for (String pairs in input) {
+    List<String> leftElf = pairs.split(",")[0].split("-");
+    List<String> rightElf = pairs.split(",")[1].split("-");
+    if ((int.parse(leftElf[0]) <= int.parse(rightElf[0])) &&
+            (int.parse(leftElf[1]) >= int.parse(rightElf[0])) ||
+        (int.parse(rightElf[0]) <= int.parse(leftElf[0])) &&
+            (int.parse(rightElf[1]) >= int.parse(leftElf[0]))) {
+      count++;
+    }
+  }
+
+  print(count);
 }

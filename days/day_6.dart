c1f9749417bf11f9a0ed3ace1ca0768db1b49096
Tim Simmonds <tim.simmonds@bjss.com>
2022-12-06 13:52:31 +0000

    day 6 touchup

--- days/day_6.dart
+++ days/day_6.dart
@@ -4,12 +4,14 @@ void main() {
   // List<String> input = sampleInput;
   List<String> input = properInput;
 
+  int bufferSize = 14;
+
   Iterable<String> letters = [];
   for (int i = 0; i < input[0].length; i++) {
-    if (letters.toSet().length == 14) {
+    if (letters.toSet().length == bufferSize) {
       print(i);
       break;
     }
-    letters = [input[0][i], ...letters].take(14);
+    letters = [input[0][i], ...letters].take(bufferSize);
   }
 }
